6f0d56e7cd2314ef1d6dad21d019726c9d13e736
Raghav0422 <kumarraghav561@gmail.com>
2025-12-28 12:00:25 +0530

    Completed ViewSet CBV working

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+
 from . import views
+
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register('employees',views.EmployeeViewSet,basename="emplo")
 
 urlpatterns = [
     path('students/', views.studentView),
@@ -7,4 +13,6 @@ urlpatterns = [
 
     path('employee/',views.EmployeeView.as_view()),
     path('employee/<int:pk>/',views.EmployeeDetailsView.as_view()),
+
+    path('',include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import Http404
 # from django.http import JsonResponse
 
@@ -14,6 +14,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import viewsets
 
 
 
@@ -59,6 +60,39 @@ def studentDetailsView(request,pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+#BELOW CLASS IS CBV FOR VIEWSET
+class EmployeeViewSet(viewsets.ViewSet):
+    def list(self,request):
+        queryset=EmployeeModel.objects.all()
+        serializer=EmployeeSerializer(queryset,many=True)
+        return Response(serializer.data)
+    
+    def create(self,request):
+        serializer=EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self,request,pk=None):
+        employee=get_object_or_404(EmployeeModel,pk=pk)
+        serializer=EmployeeSerializer(employee)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def update(self,request,pk=None):
+        employee=get_object_or_404(EmployeeModel,pk=pk)
+        serializer=EmployeeSerializer(employee,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.error)
+
+    def delete(self, request,pk=None):
+        employee=get_object_or_404(EmployeeModel,pk=pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 #BELOW CLASS IS CBV WITH GENERTICS TO VIEW ALL EMPLOYEE DATA
 class EmployeeView(generics.ListCreateAPIView):
     queryset=EmployeeModel.objects.all()
@@ -91,6 +125,7 @@ class EmployeeView(generics.ListCreateAPIView):
 #             serializer.save()
 #             return Response(serializer.data,status=status.HTTP_201_CREATED)
 #         return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+
 
 
 #BELOW CLASS IS CBV WITH GENERICS TO VIEW UPDATE DELETE SINGLE EMPOYEE DATA
